0ace244a0adad898fd753b0903f6ef98fa26e1bf
Shusaku <ishikawasyuusaku@gmail.com>
2019-05-13 21:47:40 +0900

    a

--- techpit2/amazon/urls.py
+++ techpit2/amazon/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
      path('item/<int:pk>', views.ItemDetail.as_view(), name = 'item_detail'), # 追加
      path('', views.Login.as_view(), name = "login"), # 追加
     path('logout', django_auth_views.LogoutView.as_view(), name = "logout"), # 追加
+    path('sign_up/', views.SignUp.as_view(), name='sign_up'), # [6-5]追加
+    path('sign_up/done/<token>', views.SignUpDone.as_view(), name='sign_up_done'), # [6-5]追加
 ]
